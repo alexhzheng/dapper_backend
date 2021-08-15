8f94b71e1831cc099f79b9f157dd9d520caccab5
alexhzheng <alex.h.zheng@gmail.com>
2021-08-14 22:48:33 -0700

    changed contract

--- api/opensea/[dap].js
+++ api/opensea/[dap].js
@@ -1,11 +1,11 @@
 /** @format */
 
-const mongoose = require('mongoose');
-const config = require('../../config/config');
+const mongoose = require("mongoose");
+const config = require("../../config/config");
 
-require('../../models/mintedDap');
+require("../../models/mintedDap");
 
-const Dap = mongoose.model('Dap');
+const Dap = mongoose.model("Dap");
 
 mongoose
   .connect(config.dbConnection, {
@@ -17,22 +17,22 @@ mongoose
 
 const dapGenerator = async (req, res) => {
   if (!req.query.dap) {
-    return res.send({ error: 'Could not get token id' });
+    return res.send({ error: "Could not get token id" });
   }
   const tokenId = req.query.dap;
   console.log(tokenId);
 
   const dap = await Dap.findOne({ tokenId });
   if (!dap) {
-    return res.send({ error: 'Could not fetch dap for token id' });
+    return res.send({ error: "Could not fetch dap for token id" });
   }
-  console.log('Fetched dap with name: ' + dap.name);
+  console.log("Fetched dap with name: " + dap.name);
 
   //   const colorName = 'Pantone 19-3952 Surf The Web';
-  const dapVideo = 'https://cloudflare-ipfs.com/ipfs/' + dap.ipfsImageHash;
+  const dapVideo = "https://cloudflare-ipfs.com/ipfs/" + dap.ipfsImageHash;
   const dapJson = {
-    external_url: '',
-    video: dapVideo,
+    external_url: "http://localhost:3000/dap/" + dap.ipfsImageHash,
+    image: dapVideo,
     name: dap.name,
   };
   return res.json(dapJson);
