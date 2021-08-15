01e7baeb90967202db8e44ace18714c3cd54652c
alexhzheng <alex.h.zheng@gmail.com>
2021-08-15 05:07:16 -0700

    added minter backend

--- api/dap/minter.js
+++ api/dap/minter.js
@@ -15,22 +15,20 @@ mongoose
   .catch((err) => console.log(err.reason));
 
 const minter = async (req, res) => {
-  if (!req.query.minter) {
-    return res.send({ error: "Could not get minter" });
+  if (!req.body || !req.body.minter) {
+    return res.send({ error: "dap not provided" });
   }
-  const minterAddress = req.query.minter;
-  console.log(minterAddress);
-
+  const minterAddress = req.body.minter;
   const dap = await Dap.findOne({ minterAddress });
   if (!dap) {
     return res.send({ error: "Could not fetch dap for token id" });
   }
   console.log("Fetched dap with name: " + dap.name);
 
-  const tokenId = {
+  const dapId = {
     tokenId: dap.tokenId,
   };
-  return res.send({ dap: tokenId });
+  return res.send({ dap: dapId });
 };
 
 module.exports = minter;
